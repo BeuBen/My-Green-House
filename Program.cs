ab674a5bb3b10d3b7555cac82c81196881a74958
MatteFlorian <florian.matte@isen.yncrea.fr>
2017-06-10 17:50:47 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -67,6 +67,37 @@ namespace WebTest
 
                     //STATE OBJECT CAPTEURS
                     PackageHost.PushStateObject("Résultats Capteurs", Rcapteur, lifetime: PackageHost.GetSettingValue<int>("Interval") + 4000);
+
+                    //STATE OBJECT
+                    //Un state object expire si après 4 seconde de son intervale il n'est pas mis à jour. C'est un choix arbitraire
+                    Thread.Sleep(PackageHost.GetSettingValue<int>("Interval"));
+                }
+            });
+
+        }
+    }
+}
+        }
+
+        public override void OnStart()
+        {
+            PackageHost.WriteInfo("Package starting - IsRunning: {0} - IsConnected: {1}", PackageHost.IsRunning, PackageHost.IsConnected);
+            PackageHost.WriteInfo("Je suis le package nommé {0} version {1}", PackageHost.PackageName, PackageHost.PackageVersion);
+
+
+
+            Task.Factory.StartNew(() =>
+            {
+                while (PackageHost.IsRunning)
+                {
+                    //Mettre les valeurs des capteurs dans la classe   
+                    Rcapteur.Humidité.Value = rdn.Next(10, 30);
+                    Rcapteur.Luminosité.Value = rdn.Next(10, 30);
+                    Rcapteur.Niveau_eau.Value = rdn.Next(10, 30);
+                    Rcapteur.Température.Value = rdn.Next(10, 30);
+
+                    //STATE OBJECT CAPTEURS
+                    PackageHost.PushStateObject("Résultats Capteurs", Rcapteur, lifetime: PackageHost.GetSettingValue<int>("Interval") + 4000);
                     
                     //STATE OBJECT
                     //Un state object expire si après 4 seconde de son intervale il n'est pas mis à jour. C'est un choix arbitraire
